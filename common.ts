6b5b7068186751cc843f7b7bf11328217b79a220
Godfried Meesters <godfriedmeesters@gmail.com>
2021-04-20 20:15:51 +0200

    bugfix

--- common.ts
+++ common.ts
@@ -2,7 +2,7 @@
  * @ Author: Godfried Meesters <godfriedmeesters@gmail.com>
  * @ Create Time: 2020-11-23 17:58:06
  * @ Modified by: Godfried Meesters <godfriedmeesters@gmail.com>
- * @ Modified time: 2021-04-20 20:14:35
+ * @ Modified time: 2021-04-20 20:15:38
  * @ Description:
  */
 
@@ -48,20 +48,20 @@ export async function addToQueue(job: any) {
     await webScraperCommands.add(job);
   }
   else
-    if (job.scraperClass.includes("App") && ("useRealDevice" in job.params) && yn(job.params.useRealDevice)) {
+    if (job.scraperClass.includes("App") /*&& ("useRealDevice" in job.params) && yn(job.params.useRealDevice)*/) {
       logger.info(`Adding job ${JSON.stringify(job)} to realDeviceScraperCommands`)
       await realDeviceScraperCommands.add(job);
     }
     else
-    if (job.scraperClass.includes("MobileBrowser")) {
-      logger.info(`Adding job ${JSON.stringify(job)} to mobileBrowserScraperCommands`)
-      await mobileBrowserScraperCommands.add(job);
-    }
-    else
-      if (job.scraperClass.includes("App")) {
-        logger.info(`Adding job ${JSON.stringify(job)} to emulatedDeviceScraperCommands`)
-        await emulatedDeviceScraperCommands.add(job);
+      if (job.scraperClass.includes("MobileBrowser")) {
+        logger.info(`Adding job ${JSON.stringify(job)} to mobileBrowserScraperCommands`)
+        await mobileBrowserScraperCommands.add(job);
       }
+  // else
+  //   if (job.scraperClass.includes("App")) {
+  //     logger.info(`Adding job ${JSON.stringify(job)} to emulatedDeviceScraperCommands`)
+  //     await emulatedDeviceScraperCommands.add(job);
+  //   }
 }
 
 export async function launchComparison(comparison: any) {
